b3965d48390161cb01ee9f23b2554e875f200c2b
챔래밍 <100404741+BORAMCHAM@users.noreply.github.com>
2023-06-05 17:53:36 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,22 @@
+/* 10988번 팰린드롬인지 확인하기 (C++) */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	// 단어의 길이는 1보다 크거나 같고 100보다 작거나 같음
+	string str;
+	cin >> str;
+
+	// 단어 길이를 반으로 나누어서 팰린드롬인지 확인하는 반복문
+	for (int i = 0; i < str.length() / 2; i++) {
+		// 만약 str[i]와 str[strlen(str) - 1 - i]와 다르면 팰린드롬이 아니므로
+		if (str[i] != str[str.length() - 1 - i]) {
+			cout << 0;
+			return 0;
+		}
+	}
+	cout << 1;
+	return 0;
+}
