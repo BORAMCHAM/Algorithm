56af6cd4e6b57fbf603da3e2e6f61b6a0a79c189
챔래밍 <100404741+BORAMCHAM@users.noreply.github.com>
2023-06-22 18:35:11 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
@@ -0,0 +1,24 @@
+/* 2851번 슈퍼 마리오 (C++) */
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+	// 10개의 버섯
+	int mush[10];
+	int sum = 0;
+
+	// 버섯 점수 입력 반복문
+	for (int i = 0; i < 10; i++) 
+		cin >> mush[i];
+	
+	// 마리오가 먹은 버섯 점수 반복문
+	for (int i = 0; i < 10; i++) {
+		if (abs(100 - sum) >= abs(100 - (sum + mush[i])))
+			sum += mush[i];
+		else
+			break;
+	}
+	cout << sum;
+	return 0;
+}
