b12a6241c09ccb84d3b189182918224bc9cff8ea
챔래밍 <100404741+BORAMCHAM@users.noreply.github.com>
2023-06-22 18:35:36 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,17 @@
+/* 10808번 알파벳 개수 (C++) */
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	string S;
+	cin >> S;
+
+	// 소문자 a ~ z는 아스키코드에서 97 ~ 122 
+	for (int i = 97; i <= 122; i++)
+		// count 함수 사용
+		cout << count(S.begin(), S.end(), i) << ' ';
+
+	return 0;
+}
