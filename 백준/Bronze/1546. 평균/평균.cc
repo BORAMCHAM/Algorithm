692851463d5a9bd262654e60179bd5870cd0988a
챔래밍 <100404741+BORAMCHAM@users.noreply.github.com>
2023-06-24 20:24:48 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,34 @@
+/* 1546번 평균 (C++) */
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	// 시험 본 과목 개수
+	int N;
+	cin >> N;
+
+	// 세준이의 현재 성적(점수)
+	int score[1000];
+	for (int i = 0; i < N; i++) {
+		cin >> score[i];
+	}
+
+	// 세준이 점수 중 최댓값
+	int M = *max_element(score, score + N);
+
+	double newScore[1000];
+	double sum = 0;
+	double avg = 0;
+
+	for (int i = 0; i < N; i++) {
+		// 점수를 "원래점수 / M * 100"으로 수정
+		newScore[i] = (double)score[i] / (double)M * 100;
+		sum += newScore[i];
+	}
+
+	avg = sum / N;
+	cout << avg;
+	return 0;
+}
